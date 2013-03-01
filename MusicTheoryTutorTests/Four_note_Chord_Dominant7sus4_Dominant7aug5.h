8b7a52455e7137042401b3504d808675ef7f5139
Chelsea Brzezinski <cbrzezin@asu.com>
2013-02-28 20:32:57 -0700

    More 4-note chord tests completed

---
+++ MusicTheoryTutorTests/Four_note_Chord_Dominant7sus4_Dominant7aug5.h
@@ -0,0 +1,13 @@
+//
+//  Four_note_Chord_Dominant7sus4_Dominant7aug5.h
+//  MusicTheoryTutor
+//
+//  Created by Chelsea  Brzezinski on 2/28/13.
+//  Copyright (c) 2013 Steven Lester. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Four_note_Chord_Dominant7sus4_Dominant7aug5 : SenTestCase
+
+@end
---
+++ MusicTheoryTutorTests/Four_note_Chord_Dominant7sus4_Dominant7aug5.m
@@ -0,0 +1,13 @@
+//
+//  Four_note_Chord_Dominant7sus4_Dominant7aug5.m
+//  MusicTheoryTutor
+//
+//  Created by Chelsea  Brzezinski on 2/28/13.
+//  Copyright (c) 2013 Steven Lester. All rights reserved.
+//
+
+#import "Four_note_Chord_Dominant7sus4_Dominant7aug5.h"
+
+@implementation Four_note_Chord_Dominant7sus4_Dominant7aug5
+
+@end
